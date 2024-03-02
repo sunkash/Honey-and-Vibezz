1b2940018ff81125ecb48b588fafef168d1cac8e
Dave Tresil <davetresil@daves-air.mynetworksettings.com>
2024-03-02 10:50:51 -0500

    first commit

---
+++ honey_and_vibezz/honey_and_vibezz/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for honey_and_vibezz project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'honey_and_vibezz.settings')
+
+application = get_asgi_application()
