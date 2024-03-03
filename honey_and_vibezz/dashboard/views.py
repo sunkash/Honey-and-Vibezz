3922e9ab81e6a9b5583cd52efc099aaa2e027c7f
sunkash <109238910+sunkash@users.noreply.github.com>
2024-03-03 00:16:36 -0500

    search function, fixed categories under dashboard

--- honey_and_vibezz/dashboard/views.py
+++ honey_and_vibezz/dashboard/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from item.models import Item
+from item.models import Item, Category
 from django.contrib.auth.decorators import login_required 
 # Create your views here.
 
@@ -7,9 +7,10 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def index(request):
     items = Item.objects.filter(created_by=request.user)
-
+    categories = Category.objects.all()
     return render(request, 'dashboard/index.html', {
-        'items':items
+        'items':items,
+        'categories': categories,
     })
 
 
--- honey_and_vibezz/item/views.py
+++ honey_and_vibezz/item/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Item
+from .models import Item, Category
 from .forms import NewItemForm, EditItemForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+
+
+def items(request):
+    query= request.GET.get('query', '')
+    category_id=request.GET.get('category', 0)
+    items=Item.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains = query) | Q(description__icontains = query))
+
+    return render(request, 'item/items.html', {
+        'items': items,
+        'query': query, 
+        'categories': categories,
+        'category_id': int(category_id),
+    })
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)   
