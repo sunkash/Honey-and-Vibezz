372c6eb837eaa15355c4c8509e79f4944ddfbf27
sunkash <109238910+sunkash@users.noreply.github.com>
2024-03-02 21:57:15 -0500

    dashboard, delete buttons

--- honey_and_vibezz/honey_and_vibezz/urls.py
+++ honey_and_vibezz/honey_and_vibezz/urls.py
@@ -23,5 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),  
     path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
