2cd18d2da1aab568174d09146d6ba0751fdf409d
sunkash <109238910+sunkash@users.noreply.github.com>
2024-03-02 20:15:12 -0500

    logins, logouts, submit new item forms

--- honey_and_vibezz/honey_and_vibezz/urls.py
+++ honey_and_vibezz/honey_and_vibezz/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from core.views import index, contact   
+from django.urls import path, include   
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,6 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('items/', include('item.urls')),
-    path('', index, name ='index'),
-    path('contact/', contact, name = 'contact'),
+    path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
