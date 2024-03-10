a510a37a4a2cbe083b960f503f686ce725f8b5b4
sunkash <109238910+sunkash@users.noreply.github.com>
2024-03-10 15:31:24 -0400

    title change

---
+++ crochet/core/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect 
+from item.models import Category, Item
+from .forms import SignupForm
+
+def index(request):
+    items= Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'items' : items,
+    })
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def terms(request):
+    return render(request, 'core/terms.html')
+
+def policy(request):
+    return render(request, 'core/policy.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form= SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form':form
+    })
